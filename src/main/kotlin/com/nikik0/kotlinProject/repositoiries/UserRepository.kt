2bfff3557be8626462baf20913d4dff73310cd9d
Nikik0 <nikitapolin47@gmail.com>
2023-07-26 01:03:36 +1000

    added getBetween method for users

--- src/main/kotlin/com/nikik0/kotlinProject/repositoiries/UserRepository.kt
+++ src/main/kotlin/com/nikik0/kotlinProject/repositoiries/UserRepository.kt
@@ -1,10 +1,13 @@
 package com.nikik0.kotlinProject.repositoiries
 
 import com.nikik0.kotlinProject.entities.UserEntity
+import kotlinx.coroutines.flow.Flow
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository: CoroutineCrudRepository<UserEntity, Long> {
     //fun deleteById(id: Long): Boolean
+
+    fun getAllByAgeBetween(lowerAge: Int, upperAge: Int): Flow<UserEntity>
 }
